fce632bb90904a23a4f2f5c8150a4fc48ae2b8a5
David Richer <104082608+richerdavid@users.noreply.github.com>
2022-11-10 14:22:20 -0500

    v1

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # sama-setup
+
+
+## Quick start
+
+Print temporary credentials
+```
+python3 sama-aws.py --credential-process --project-id 9888
+```
+
+## Add a sama profile in ~/.aws/config
+
+```
+[profile sama]
+credential_process = python3 /<absolute-path>/sama-aws.py --project-id -i 9888
+```
+
+Test it
+```
+aws sts get-caller-identity --profile sama
+```
---
+++ sama-aws.py
@@ -0,0 +1,63 @@
+import argparse
+import configparser
+import json
+from pathlib import Path
+import os
+import time
+import urllib.request
+
+def get_temp_credentials(project_id):
+
+    f = open(os.path.join(Path.home(), '.sama.json'), "r")
+    sama_config = json.load(f)
+
+    # open a connection to a URL using urllib
+    resp  = urllib.request.urlopen("https://api.sama.com/v2/projects/%s/credentials.json?access_key=%s" % (project_id, sama_config['apiKey']))
+    return json.load(resp)
+
+def refresh_credentials(profile, project_id):
+    print('refreshing %s credentials' % (profile))
+
+    credentials_file = os.path.join(Path.home(), '.aws/credentials')
+    config = configparser.ConfigParser()
+    config.read(credentials_file)
+
+    temp_creds = get_temp_credentials(project_id)
+
+    config[profile]['aws_access_key_id'] = temp_creds['access_key_id']
+    config[profile]['aws_secret_access_key'] = temp_creds['secret_access_key']
+    config[profile]['aws_session_token'] = temp_creds['session_token']
+
+    with open(credentials_file, 'w') as configfile:
+        config.write(configfile)
+
+
+parser = argparse.ArgumentParser(
+                    prog = 'sama-aws',
+                    description = 'Get Sama temporary credentials')
+
+parser.add_argument('-c', '--credential-process',
+                    action='store_true')
+
+parser.add_argument('-i', '--project-id', type=int, required=True, help="The Sama project id")
+parser.add_argument('-p', '--profile', help="The AWS CLI profile to refresh in ~/.aws/credentials")
+
+args = parser.parse_args()
+
+if(args.credential_process):
+    temp_creds = get_temp_credentials(args.project_id)
+
+    output = {
+        "Version": 1,
+        "AccessKeyId": temp_creds['access_key_id'],
+        "SecretAccessKey": temp_creds['secret_access_key'],
+        "SessionToken": temp_creds['session_token'],
+        "Expiration": temp_creds['expiration'],
+    }
+    print(json.dumps(output))
+    
+if(args.profile):
+    while(True):
+        refresh_credentials(args.profile, args.project_id)
+        print('Sleeping for 45 minutes...')
+        time.sleep(2700)
